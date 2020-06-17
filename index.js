a8cacfe253c11015a90a8b4d7c62d14f580c450b
Jiro Liu <congtien.lao@gmail.com>
2020-06-17 19:20:19 +0800

    4. POST method

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 3000;
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 var users = [
     {id: 1, name: "Tuan"},
     {id: 2, name: "Tu"}
@@ -26,13 +29,23 @@ app.get('/users/search', function(req, res){
     var query = req.query.q;
     var matchName = users.filter(function (user) {
         return user.name.toLowerCase().indexOf(query.toLowerCase()) != -1;
-    })
+    });
     // console.log(req.query);
     res.render('users',{
         users: matchName,
         query: query
-    })
+    });
 })
+
+app.get('/users/create', function (req, res) {
+    res.render('users/create');
+});
+
+app.post('/users/create', function (req, res)    {
+    // console.log(req.body);
+    users.push(req.body);
+    res.redirect('/users');
+});
 
 app.listen(port, function(){
     console.log('Express listening in port: ' + port);
