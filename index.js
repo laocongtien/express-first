2694338150848ddb1ca611a17651ad6fded01d65
Jiro Liu <congtien.lao@gmail.com>
2020-06-17 18:30:13 +0800

    Query parameters for search

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const port = 3000;
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+var users = [
+    {id: 1, name: "Tuan"},
+    {id: 2, name: "Tu"}
+];
+
 app.get('/',function(req, res){
     res.render('index',{
         name: 'AAA'
@@ -13,12 +18,21 @@ app.get('/',function(req, res){
 
 app.get('/users',function(req, res){
     res.render('users/index',{
-        users:[
-            {id: 1, name: "Tuan"},
-            {id: 2, name: "Tu"}
-        ]
+        users: users
     });
 });
+
+app.get('/users/search', function(req, res){
+    var query = req.query.q;
+    var matchName = users.filter(function (user) {
+        return user.name.toLowerCase().indexOf(query.toLowerCase()) != -1;
+    })
+    // console.log(req.query);
+    res.render('users',{
+        users: matchName,
+        query: query
+    })
+})
 
 app.listen(port, function(){
     console.log('Express listening in port: ' + port);
